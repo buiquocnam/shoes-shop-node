6f507cd1b36b03a614ef8432477eee2f9d63bc45
khanh921104 <lecongkhanh54264@gmail.com>
2025-11-25 15:39:38 +0700

    order

--- services/order-service/src/routes/index.ts
+++ services/order-service/src/routes/index.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import {  getOrder, createOrder, getOrdersByUser, getOrderDetail, updatePaymentStatus, updateShippingStatus, addShippingLog} from '../controllers/orderController';
+import { get } from 'http';
 
 const router = Router();
 
 router.get('/list', (req, res) => {
   res.json({ orders: [] });
 });
+ 
+router.post("/orders", createOrder);
+router.get("/orders", getOrder);
+router.get("/orders/user/:userId", getOrdersByUser);
+router.get("/orders/:orderId", getOrderDetail);
+router.patch("/orders/:orderId/payment", updatePaymentStatus);
+router.patch("/orders/:orderId/shipping", updateShippingStatus);
+router.post("/orders/:orderId/shipping-log", addShippingLog);
 
 export default router;
